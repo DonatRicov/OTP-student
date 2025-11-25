425225b0ea7dcc13f8c12cbef1c620319bed9484
DESKTOP-9UB459C\Donat <dricov21@student.foi.hr>
2025-11-25 02:04:32 +0100

    Bug fixes.

--- Android/app/build.gradle.kts
+++ Android/app/build.gradle.kts
@@ -51,12 +51,14 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     implementation(platform("com.google.firebase:firebase-bom:34.5.0"))
+    implementation("com.google.android.material:material:1.12.0")
     implementation("com.google.firebase:firebase-auth")
     implementation("com.google.firebase:firebase-firestore")
     implementation("com.google.firebase:firebase-storage")
     implementation("com.google.firebase:firebase-analytics")
     implementation("androidx.viewpager2:viewpager2:1.0.0")
     implementation("de.hdodenhof:circleimageview:3.1.0")
+    implementation ("com.github.bumptech.glide:glide:4.16.0")
 
 }
 
