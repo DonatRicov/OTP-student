048a7bab6f4877b03c32227d5fd66ee9de4c7c93
DESKTOP-9UB459C\Donat <dricov21@student.foi.hr>
2025-11-25 22:45:06 +0100

    Profile logic added. Logic to be implemented.

---
+++ Android/app/src/main/java/hr/foi/air/otpstudent/ProfileSetupActivity.kt
@@ -0,0 +1,12 @@
+package hr.foi.air.otpstudent
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ProfileSetupActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile_setup)
+    }
+}
