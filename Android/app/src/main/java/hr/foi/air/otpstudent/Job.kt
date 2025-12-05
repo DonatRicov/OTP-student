502217884751aaa68d241b2df7a3bd85f4f8e130
DESKTOP-9UB459C\Donat <dricov21@student.foi.hr>
2025-12-05 21:04:52 +0100

    Job databse model.

---
+++ Android/app/src/main/java/hr/foi/air/otpstudent/Job.kt
@@ -0,0 +1,19 @@
+package hr.foi.air.otpstudent.model
+
+import com.google.firebase.Timestamp
+
+data class Job(
+    val id: String = "",
+    val title: String = "",
+    val company: String = "",
+    val location: String = "",
+    val hourlyRate: Double = 0.0,
+    val hourlyRateMax: Double = 0.0,
+    val applicantsCount: Int = 0,
+    val postedAt: Timestamp? = null,
+    val expiresAt: Timestamp? = null,
+    val isClosed: Boolean = false,
+    val isApplied: Boolean = false,
+    val isFavorite: Boolean = false,
+    val description: String = ""
+)
