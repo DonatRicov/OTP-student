39a279b101acf8aec0f653cfa5108cbf2c3791ea
DESKTOP-9UB459C\Donat <dricov21@student.foi.hr>
2025-12-24 17:38:10 +0100

    feat(internships/data): added InternshipRepository as single source of internship data - Coordinates between remote source and cache

---
+++ Android/app/src/main/java/hr/foi/air/otpstudent/domain/repository/InternshipRepository.kt
@@ -0,0 +1,15 @@
+package hr.foi.air.otpstudent.domain.repository
+
+import hr.foi.air.otpstudent.domain.model.Internship
+
+interface InternshipRepository {
+    suspend fun getInternshipsForUser(userId: String?): List<Internship>
+    suspend fun getAppliedInternshipsForUser(userId: String): List<Internship>
+    suspend fun getInternshipById(internshipId: String): Internship?
+    suspend fun setFavorite(userId: String, internshipId: String, favorite: Boolean)
+    suspend fun markApplied(userId: String, internshipId: String)
+
+    suspend fun isFavorite(userId: String, internshipId: String): Boolean
+    suspend fun isApplied(userId: String, internshipId: String): Boolean
+
+}
