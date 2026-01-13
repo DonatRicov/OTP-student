0b0681d70fffc5a9cf11039fa39cdaebeb5154da
DonatRicov <dricov21@student.foi.hr>
2026-01-13 22:38:04 +0100

    refactor(points): refactor rewards placeholder screen
    - Enable segment tab switching between challenges and rewards

--- Android/app/src/main/java/hr/foi/air/otpstudent/ui/points/RewardsFragment.kt
+++ Android/app/src/main/java/hr/foi/air/otpstudent/ui/points/RewardsFragment.kt
@@ -1,11 +1,11 @@
-package hr.foi.air.otpstudent.ui.rewards
+package hr.foi.air.otpstudent.ui.points
 
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import hr.foi.air.otpstudent.R
 
-class RewardsFragment : Fragment(R.layout.activity_rewards) {
+class RewardsFragment : Fragment(R.layout.fragment_rewards) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         // TODO: setup logike za nagrade
