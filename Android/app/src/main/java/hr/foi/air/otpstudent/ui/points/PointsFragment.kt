d1c75d90c97e4fb418e7b1907a5294e88760072d
DonatRicov <dricov21@student.foi.hr>
2026-01-13 22:39:17 +0100

    refactor(points): make PointsFragment a tab container
    - Split constant header/segment/filter UI from tab content
    
    - Use childFragmentManager to swap Challenges/Rewards screens

--- Android/app/src/main/java/hr/foi/air/otpstudent/ui/points/PointsFragment.kt
+++ Android/app/src/main/java/hr/foi/air/otpstudent/ui/points/PointsFragment.kt
@@ -1,6 +1,59 @@
 package hr.foi.air.otpstudent.ui.points
 
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import hr.foi.air.otpstudent.R
 
-class PointsFragment : Fragment(R.layout.fragment_points)
+class PointsFragment : Fragment(R.layout.fragment_points) {
+
+    private lateinit var tabChallenges: TextView
+    private lateinit var tabRewards: TextView
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        tabChallenges = view.findViewById(R.id.tabChallenges)
+        tabRewards = view.findViewById(R.id.tabRewards)
+
+        if (savedInstanceState == null) {
+            showChallenges()
+            setSelectedTab(isChallenges = true)
+        }
+
+        tabChallenges.setOnClickListener {
+            showChallenges()
+            setSelectedTab(isChallenges = true)
+        }
+
+        tabRewards.setOnClickListener {
+            showRewards()
+            setSelectedTab(isChallenges = false)
+        }
+    }
+
+    private fun showChallenges() {
+        childFragmentManager.beginTransaction()
+            .replace(R.id.pointsFragmentContainer, ChallengesFragment())
+            .commit()
+    }
+
+    private fun showRewards() {
+        childFragmentManager.beginTransaction()
+            .replace(R.id.pointsFragmentContainer, RewardsFragment())
+            .commit()
+    }
+
+    private fun setSelectedTab(isChallenges: Boolean) {
+        tabChallenges.setBackgroundResource(
+            if (isChallenges) R.drawable.bg_segment_selected else R.drawable.bg_segment_unselected
+        )
+        tabRewards.setBackgroundResource(
+            if (isChallenges) R.drawable.bg_segment_unselected else R.drawable.bg_segment_selected
+        )
+
+        tabChallenges.isClickable = !isChallenges
+        tabRewards.isClickable = isChallenges
+    }
+}
