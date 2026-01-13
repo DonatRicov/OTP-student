557a9e8fa18078fd9d72a934eea437321069b08c
Dino Primorac <dino.primorac2@gmail.com>
2026-01-13 23:04:16 +0100

    feat(chatbot): add header navigation actions (home + back) in ChatbotFragment

--- Android/app/src/main/java/hr/foi/air/otpstudent/ChatbotFragment.kt
+++ Android/app/src/main/java/hr/foi/air/otpstudent/ChatbotFragment.kt
@@ -1,59 +1,24 @@
 package hr.foi.air.otpstudent
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import android.widget.ImageButton
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+class ChatbotFragment : Fragment(R.layout.fragment_chatbot) {
 
-/**
- * A simple [Fragment] subclass.
- * Use the [ChatbotFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class ChatbotFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+        val header = view.findViewById<View>(R.id.headerChatbot)
+
+        header.findViewById<ImageButton>(R.id.btnHome).setOnClickListener {
+            findNavController().popBackStack(R.id.nav_home, false)
         }
-    }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_chatbot, container, false)
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ChatbotFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ChatbotFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        header.findViewById<ImageButton>(R.id.btnBack).setOnClickListener {
+            findNavController().navigateUp()
+        }
     }
 }
--- Android/app/src/main/java/hr/foi/air/otpstudent/MainActivity.kt
+++ Android/app/src/main/java/hr/foi/air/otpstudent/MainActivity.kt
@@ -1,6 +1,7 @@
 package hr.foi.air.otpstudent
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
@@ -16,10 +17,19 @@ class MainActivity : AppCompatActivity() {
         FirebaseApp.initializeApp(this)
 
         val bottomNav = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
-        val navHostFragment = supportFragmentManager
-            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
 
         bottomNav.setupWithNavController(navController)
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            bottomNav.visibility = if (destination.id == R.id.chatbotFragment) {
+                View.GONE
+            } else {
+                View.VISIBLE
+            }
+        }
     }
 }
